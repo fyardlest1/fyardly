c2e8f4fb4a4aa4fc1253a4adaf35e8927310b151
Fyardlest <fyardlest1@gmail.com>
2020-09-06 11:02:39 -0400

    Single Page Component OK

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Counter from './components/counter'
+
+function App() {
+  return (
+    <main class="container">
+      <Counter />
+    </main>
+  );
+}
+
+export default App;
